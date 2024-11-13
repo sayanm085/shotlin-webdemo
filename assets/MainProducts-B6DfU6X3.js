d85bbc6794a84e9f3620fd7fddbe3583973b6b98
Sayan Mondal <sayanm085@gmail.com>
2024-11-13 15:46:48 +0530

    bug

---
+++ assets/MainProducts-B6DfU6X3.js
@@ -0,0 +1,1 @@
+import{j as t,Q as r}from"./index-D_qb8161.js";function e(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{e as default};
--- assets/MainProducts-DsXPlRev.js
+++
@@ -1,1 +0,0 @@
-import{j as t,R as r}from"./index-BYtLH1kR.js";function e(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{e as default};
